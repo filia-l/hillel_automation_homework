217181182a723c90dba7fb56aa95002fdb2ff1e7
Liubov Moshynska <lmoshynska@intellias.com>
2019-06-20 12:52:38 +0300

    minor change

--- src/test/java/lesson_4_5_hw_tests/BaseTest.java
+++ src/test/java/lesson_4_5_hw_tests/BaseTest.java
@@ -21,7 +21,7 @@ public class BaseTest {
 
     @Before
     public void driverSetUp(){
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         webDriver.manage().window().maximize();
